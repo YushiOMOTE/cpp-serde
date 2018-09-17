0f4ffb18b042e47b77379913fabb8c316fd1fad8
Yushi OMOTE <yushiomote@gmail.com>
2018-09-17 22:06:03 +0900

    Add header which includes all serde lang plugins at once

---
+++ include/serde/serde_all.h
@@ -0,0 +1,13 @@
+#ifndef SERDE_ALL_H_
+#define SERDE_ALL_H_
+
+#include <serde/serde.h>
+
+#include <serde/cbor.h>
+#include <serde/json.h>
+#include <serde/msgpack.h>
+#include <serde/toml.h>
+#include <serde/ubjson.h>
+#include <serde/yaml.h>
+
+#endif // SERDE_ALL_H_
